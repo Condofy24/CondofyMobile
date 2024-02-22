8596537d6c4cd965e0d5a6c115825ce176d0074b
Ramy Attalla <83047251+ramy-att@users.noreply.github.com>
2024-02-21 22:50:46 -0500

    add redux and login actions

---
+++ types.ts
@@ -0,0 +1,32 @@
+export interface UserInfo {
+    email: string;
+    id: string;
+    name: string;
+    role: UserRolesEnum;
+    phoneNumber: string;
+    imageUrl: string;
+    imageId: string;
+  }
+  
+  export enum UserRolesEnum {
+    MANAGER = 0,
+    STAFF = 1,
+    ACCOUNTANT = 2,
+    OWNER = 3,
+    RENTER = 4,
+  }
+  
+  export type PropertyInformation = {
+    name: string;
+    address: string;
+    units: number;
+    parking: number;
+    storage: number;
+  };
+  
+  export type UnitInformation = {
+    unitNumber: number;
+    size: number;
+    isOccupied: boolean;
+    fees: number;
+  };
