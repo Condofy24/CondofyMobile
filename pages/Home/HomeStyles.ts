29eb84f5481a11084caa5ffc5cf8b0292e532515
jp503 <ares90jeff@gmail.com>
2024-03-19 00:17:22 -0400

    added home page

---
+++ pages/Home/HomeStyles.ts
@@ -0,0 +1,35 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    homePage: {
+        flex: 1,
+        height: "100%"
+    },
+    selectionContainer: {
+        height: "50%",
+        textAlign: "center"
+    },
+    textContainer: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    imageContainer: {
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        resizeMode: "cover",
+    },
+    textStyle: {
+        fontWeight: "bold"
+    },
+    overlay: {
+        flex: 1,
+        backgroundColor: 'rgba(255, 255, 255, 0.4)', // Semi-transparent overlay
+    },
+    horizontalLine: {
+        height:2,
+        backgroundColor: "black",
+        width: "100%"
+    }
+});
